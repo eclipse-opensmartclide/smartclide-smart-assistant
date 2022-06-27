3ed53c0c69056a536670bff5895c3e201e9741a2
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-27 15:52:19 +0200

    Adding missing copyrights
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- smartclide-dle/smartclide-dle/smartclide_dle/iamodeler/templates/server.py
+++ smartclide-dle/smartclide-dle/smartclide_dle/iamodeler/templates/server.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+#*******************************************************************************
+# Copyright (C) 2021-2022 AIR Institute
+# 
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+# 
+# SPDX-License-Identifier: EPL-2.0
+#*******************************************************************************
+
 import pickle
 
 import pandas as pd
